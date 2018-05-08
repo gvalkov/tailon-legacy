e7c963fa088e4a5a12ce9f0127b955a25bda0693
Georgi Valkov <georgi.t.valkov@gmail.com>
2018-05-08 21:02:52 +0200

    Bump version: 1.3.0 -> 1.4.0

--- docs/conf.py
+++ docs/conf.py
@@ -25,7 +25,7 @@ master_doc = 'index'
 project = u'tailon'
 copyright = u'2013-2017, Georgi Valkov'
 
-release = '1.3.0'
+release = '1.4.0'
 version = release
 
 exclude_patterns = ['_build']
--- setup.py
+++ setup.py
@@ -19,7 +19,7 @@ requirements = [
 
 kw = {
     'name':             'tailon',
-    'version':          '1.3.1',
+    'version':          '1.4.0',
     'description':      'Webapp for looking at and searching through log files',
     'long_description': open('README.rst').read(),
     'author':           'Georgi Valkov',
